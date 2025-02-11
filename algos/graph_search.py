ecb04644a212d905875382dee0407763b8f2da7c
thmaz <thomasplmn@gmail.com>
2025-02-11 19:23:50 +0100

    graph search

--- algos/graph_search.py
+++ algos/graph_search.py
@@ -37,30 +37,21 @@ class FIFO:
 
 # This class represents an undirected graph
 class Graph:
- 
     def __init__(self):
         self.graph = defaultdict(list)      # init dictionary of empty lists
  
     # Function to add an undirected edge to graph
     def add_edge(self, u: int, v: int):
-        ##############################
         ### add an edge by appending v to the list of graph[u] and vice versa 
-        ### write code here
-
-        self.graph[u].append[v]
-        self.graph[v].append[u]
-        
-        ##############################
+        self.graph[u].append(v)
+        self.graph[v].append(u)
         pass
     
     def traversal(self, initial_node: int, algorithm='bfs'):
         
         frontier = None
-        ##############################
         ### if algorithm is 'bfs', initialize frontier as a FIFO
         ### if algorithm is 'dfs', initialize frontier as a LIFO
-        ### write code here
-        
         if algorithm == 'bfs':
             frontier = FIFO()
         elif algorithm == 'dfs':
@@ -82,6 +73,14 @@ class Graph:
                 ###     append it in the reached list
                 ###     push child into FIFO/LIFO queue
                 ### write code here
-                if child != 
+                if child not in reached:
+                    reached.append(child)
+                    frontier.push(child)
                 ##############################
                 pass
+
+g = Graph()
+g.add_edge(2, 3);  g.add_edge(3, 4);  g.add_edge(3, 7);  g.add_edge(7, 6)
+g.add_edge(6, 5);  g.add_edge(7, 11); g.add_edge(7, 0);  g.add_edge(0, 9)
+g.add_edge(0, 11); g.add_edge(9, 9);  g.add_edge(9, 10); g.add_edge(10, 1)
+g.add_edge(1, 9);  g.add_edge(9, 12); g.add_edge(12, 2)
