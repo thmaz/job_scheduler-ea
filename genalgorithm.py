102e2332ebc4a32492b99da1d6d107f4313a20c1
thmaz <thomasplmn@gmail.com>
2025-02-25 13:02:23 +0100

    added defs

--- genalgorithm.py
+++ genalgorithm.py
@@ -1,7 +1,7 @@
-import numpy as np
 import random
+import numpy as np
 
-TARGET = 'Artificial intelligence'
+TARGET = 'Artificial Intelligence'
 
 def compute_fitness(value: str, target: str):
     ### for each character in the value/target string
@@ -9,6 +9,32 @@ def compute_fitness(value: str, target: str):
     fitness = 0
     for val, tgt in zip(value, target):
         fitness += val == tgt
-    return fitness
+        return fitness
 
-assert compute_fitness('    cif hello world', TARGET) == 3
+class Agent:
+    def __init__(self, value: str) -> None:
+        self.value = value
+
+class Population:
+    GENES = '''abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOP
+                QRSTUVWXYZ 1234567890, .-;:_!"#%&/()=?@${[]}'''
+
+    def __init__(self, n_pop: int, max_len: int) -> None:
+        self.n_pop = n_pop
+        self.max_len = max_len
+        self.agents = []
+
+    def random_string(self):
+        # Return random sample from "GENES"
+        return ''.join(random.choice(self.GENES) for _ in range(self.max_len))
+
+    def populate(self):
+        # Populate list with random strings
+        self.agents = [Agent(self.random_string()) for _ in range(self.max_len)]
+        pass
+
+    def evaluate(self, target):
+        
+        pass
+
+print("finished ...")
